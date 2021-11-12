bf96526fa753a4fe151cd80bc508e93ea69423d6
rahultolani703 <94065719+rahultolani703@users.noreply.github.com>
2021-11-12 22:21:52 +0530

    Add files via upload

---
+++ database/banking_db.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 12, 2021 at 05:38 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banking_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(30) NOT NULL,
+  `account_number` varchar(50) NOT NULL,
+  `pin` text NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `middlename` varchar(250) NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `generated_password` text NOT NULL,
+  `balance` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `account_number`, `pin`, `firstname`, `lastname`, `middlename`, `email`, `password`, `generated_password`, `balance`, `date_created`, `date_updated`) VALUES
+(5, '1000', '425401', 'Rahul', 'Tolani', 'Nanakram', 'rahultolani703@gmail.com', 'd41d8cd98f00b204e9800998ecf8427e', '', 2500, '2021-11-12 12:39:55', '2021-11-12 22:04:44'),
+(6, '1001', '425402', 'Sahil', 'Tolani', 'Mulchand ', 'sahiltolani@gmail.com', '', 'Sahil@123', 5000, '2021-11-12 12:41:24', '2021-11-12 12:49:04'),
+(7, '1003', '425403', 'Dheeraj', 'Tolani', 'Mulchand ', 'dheerajtolani@gmail.com', '', 'Dheeraj@123', 5500, '2021-11-12 12:48:17', '2021-11-12 12:50:08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+CREATE TABLE `announcements` (
+  `id` int(30) NOT NULL,
+  `title` text NOT NULL,
+  `announcement` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `announcements`
+--
+
+INSERT INTO `announcements` (`id`, `title`, `announcement`, `date_created`, `date_updated`) VALUES
+(4, 'Holidays', 'On Sunday Banks are Close', '2021-11-12 09:57:58', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
+
+CREATE TABLE `system_info` (
+  `id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_info`
+--
+
+INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
+(1, 'name', ' Amalner Online Banking System'),
+(6, 'short_name', 'AOBS'),
+(11, 'logo', 'uploads/1626243720_bank.png'),
+(13, 'user_avatar', 'uploads/user_avatar.jpg'),
+(14, 'cover', 'uploads/1626249540_dark-bg.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(30) NOT NULL,
+  `account_id` int(30) NOT NULL,
+  `type` tinyint(4) NOT NULL COMMENT '1=Cash in, 2= Withdraw, 3=transfer',
+  `amount` float NOT NULL,
+  `remarks` text DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `account_id`, `type`, `amount`, `remarks`, `date_created`) VALUES
+(22, 5, 1, 1000, 'Beginning balance', '2021-11-12 12:39:55'),
+(23, 6, 1, 2000, 'Beginning balance', '2021-11-12 12:41:24'),
+(24, 7, 1, 5000, 'Beginning balance', '2021-11-12 12:48:17'),
+(25, 6, 1, 3000, 'Deposits', '2021-11-12 12:49:04'),
+(26, 5, 1, 2000, 'Deposits', '2021-11-12 12:49:41'),
+(27, 5, 3, 500, 'Transferred to 1003', '2021-11-12 12:50:08'),
+(28, 7, 3, 500, 'Transferred from 1000', '2021-11-12 12:50:08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
+(1, 'Adminstrator', 'Admin', 'admin', 'e6e061838856bf47e1de730719fb2609', 'uploads/1624240500_avatar.png', NULL, 1, '2021-01-20 14:02:37', '2021-11-12 09:55:16');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_info`
+--
+ALTER TABLE `system_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `system_info`
+--
+ALTER TABLE `system_info`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
